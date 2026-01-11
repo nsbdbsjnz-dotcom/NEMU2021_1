484dcc1668f2faeaba0bd86e9d1ce5c3fcce64bc
tracer-ics2016 <tracer@njuics.org>
2026-01-11 15:25:47 +0800

    >  compile NEMU 10602121 lhy Linux lhy130360 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:25:47 up 19:40,  1 user,  load average: 0.34, 0.51, 0.52 99722b9aa577b0f36f0bab11243e2eca97865041

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -4,16 +4,16 @@
 
 static void do_execute() {
     // For direct jumps, op_src->val is the offset from the next instruction
-    // So we need to calculate the correct target address
-    cpu.eip = op_src->val;
-    print_asm(str(instr) " %x", cpu.eip + DATA_BYTE + 1);
+    // So we need to add it to the current eip
+    cpu.eip += op_src->val;
+    print_asm(str(instr) " %x", cpu.eip + 1);
 }
 
 make_instr_helper(si)
 #if DATA_BYTE == 4
 make_helper(jmp_rm_l) {
     int len = decode_rm_l(eip + 1);
-    cpu.eip = op_src->val;
+    cpu.eip = op_src->val - (len + 1);
     print_asm(str(instr) " *%s", op_src->str);
     return len + 1;
 }
